485abf08f4802fc6402147ea054de1e6584a7ae9
Gabriel Ramirez <68472070+GabrielRamirezDev@users.noreply.github.com>
2020-09-20 21:23:14 -0400

    Savage 21 Full Stack Fan Site

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://dbUser:he11oworld@cluster0.g6oo7.mongodb.net/Cluster0', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'demo'
+};
